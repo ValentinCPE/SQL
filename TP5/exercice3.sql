27fcce67fea96cbb903e6ddf7a065168f51dfac6
Valentin <valentin.guevara@live.com>
2016-05-26 21:30:45 +0200

    exercice 3 TP 5

--- TP5/exercice3.sql
+++ TP5/exercice3.sql
@@ -1,6 +1,18 @@
-CREATE OR REPLACE PROCEDURE geneliste IS
-emp12 EMPLOYEES%ROWTYPE;
+set serveroutput on;
+
+CREATE OR REPLACE PROCEDURE generliste AS
+  CURSOR c_emp IS
+  SELECT * FROM EMPLOYEES;
+  CURSOR c_man (man NUMBER) IS
+  SELECT FIRST_NAME FROM EMPLOYEES WHERE EMPLOYEE_ID=man;
 BEGIN
-  SELECT * INTO emp12 FROM EMPLOYEES;
-  DBMS_OUTPUT.PUT_LINE(emp12.LAST_NAME);
+  DBMS_OUTPUT.PUT_LINE('Nom de famille' || ' | ' || 'Prénom' || '    |    ' || 'Chef');
+  DBMS_OUTPUT.PUT_LINE('-----------------------------------------');
+  FOR v_emp IN c_emp LOOP
+  FOR v_man IN c_man(v_emp.MANAGER_ID) LOOP
+  DBMS_OUTPUT.PUT_LINE('   ' || v_emp.FIRST_NAME || '   |    ' || v_emp.LAST_NAME || '     |    ' || v_man.FIRST_NAME);
+  END LOOP;
+ END LOOP; 
 END;  
+
+EXEC generliste();
